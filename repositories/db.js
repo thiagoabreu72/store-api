0476f81c600a085eb58ccde808a53a2af8911477
Thiago Abreu <thiagoabreu72@gmail.com>
2021-09-15 18:05:51 -0300

    implementado os produtos e vendas na API

--- repositories/db.js
+++ repositories/db.js
@@ -4,10 +4,9 @@ async function connect() {
   if (global.connection) {
     return global.connection.connect();
   }
-  
+
   const pool = new pg.Pool({
-    connectionString:
-      "postgres://oowzuhel:v87VsS0tSvJ7RTwiP3Awwo6fK_lyP_oZ@chunee.db.elephantsql.com/oowzuhel",
+    connectionString: process.env.DB_POSTGRE,
   });
   global.connection = pool;
   return pool.connect();
